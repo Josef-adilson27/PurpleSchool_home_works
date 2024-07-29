d4639957823f7ae55e9b4bc7f0ea4bfd2e43fcba
Yusuf <itrexus69@gmail.com>
2024-07-29 22:36:53 +0300

    7-converter added

---
+++ 7-converter/index.js
@@ -0,0 +1,32 @@
+function currencyConverter(amount, currency, targetingCurrency) {
+    const USD = 95;
+    const EUR = 110;
+    const RUB = 70;
+  
+    const  TargetingCurrency = targetingCurrency.toLowerCase().slice(0, 3)
+    const  Currency = currency.toLowerCase().slice(0, 3)
+    
+    if (Currency === "rub") {
+      switch (TargetingCurrency) {
+        case "dol":return amount * USD;
+        case "eur":return amount * EUR;
+        default:return null;
+      }
+    } else if (Currency  === "eur") {
+      switch (TargetingCurrency) {
+        case "dol":return amount * USD;
+        case "rub":return amount * RUB;
+        default:return null;
+      }
+  
+    } else if (Currency === "dol") {
+      switch (TargetingCurrency) {
+        case "eur":return amount * USD;
+        case "rub":return amount * RUB;
+        default:return null;
+      }
+    }
+  }
+  
+  console.log(currencyConverter(1200, "doll", "rub"));
+  
